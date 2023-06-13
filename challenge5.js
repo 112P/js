7241c2a99ddbcba9bf942827afa7c7bfd6dadddf
Vitor Ponce <vitor.ponce20@gmail.com>
2023-06-12 22:30:30 -0300

    [2] operadores de comparacao

---
+++ challenge5.js
@@ -0,0 +1,6 @@
+const myArray = [20, 30, 40];
+
+/* WHY THIS CODE DON'T WORK ? 
+console.log(myArray[3]) */
+
+console.log(myArray[2])
---
+++ comparisonOperators.js
@@ -0,0 +1,12 @@
+const result = 20 === 20 ? 'values match' : 'values not match';
+console.log(result);
+
+if (20 === 20) {
+    console.log('values match')
+} else {
+    console.log('values not match')
+}
+
+const result1 = 20 === 20 && 10 === 10 ? 'values match' : 'values not match';
+console.log(result1)
+
