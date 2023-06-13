f6104e6f7c605f355bd3cd631b9c3221ec0a1d24
Vitor Ponce <vitor.ponce20@gmail.com>
2023-06-13 15:39:13 -0300

    [3] challenge 2 diferenca entre constante e variavel const e let ou var

---
+++ operators-challenge1.js
@@ -0,0 +1,13 @@
+/* NAO FUNCIONA PQ O CONST NAO PERMITE ALTERAR O VALOR
+const numberVariable = 0; */
+
+let numberVariable = 0;
+
+numberVariable++;
+numberVariable++;
+numberVariable++;
+
+console.log(numberVariable);
+
+
+ 
