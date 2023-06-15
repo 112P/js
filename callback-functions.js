a039f59bc31593a475ec5fe3b05dc71fa589e5d2
Vitor Ponce <vitor.ponce20@gmail.com>
2023-06-15 13:27:28 -0300

    [6] Callbacks functions with arrow functions

--- callback-functions.js
+++ callback-functions.js
@@ -17,3 +17,20 @@ console.log("Iniciando o usarNumeroComOuSemCallback com deveUsar = true")
 console.log(usarNumeroComOuSemCallback(20, true, callbackFunctions))
 console.log("Iniciando o usarNumeroComOuSemCallback com deveUsar = false")
 console.log(usarNumeroComOuSemCallback(20, false, callbackFunctions))
+
+console.log("Iniciando o usarNumeroComOuSemCallback com nova function")
+console.log(usarNumeroComOuSemCallback(20, false, function (num){
+    return num * 2
+}))
+
+console.log("Iniciando o usarNumeroComOuSemCallback com arrow function")
+console.log(usarNumeroComOuSemCallback(20, false, (num) =>{
+    return num * 2
+}))
+
+
+console.log("Iniciando o usarNumeroComOuSemCallback com arrow function without arrow")
+console.log(usarNumeroComOuSemCallback(20, false, num => num * 2))
+
+console.log("Iniciando o usarNumeroComOuSemCallback com arrow function without arrow")
+console.log(usarNumeroComOuSemCallback(20, true, num => num * 2))
