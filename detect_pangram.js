c0b3ab8fb63e3e8f633c3122987c893c1d1fab6d
Vitor Ponce <vitor.ponce20@gmail.com>
2023-06-18 22:35:05 -0300

    [7] verificacao pangrans

---
+++ detect_pangram.js
@@ -0,0 +1,27 @@
+var string = "The quick brown fox jumps over the lazy dog."
+console.log(string)
+
+function isPangram(string){
+    //... CRIAR UM VETOR QUE VERIFICA TODAS AS LETRAS
+    const isPagran = true;
+    const notIsPagran = false;
+    let vetor = [];
+    vetor = string.split();
+    
+    for (i = 0; i < string.length; i++){
+      if (vetor[i] === 'a') {
+        console.log("Tem a");
+      } if (vetor[i] === 'b') {
+        console.log("Tem b")
+      }else {
+        
+      } vetor[i]
+      
+    }    
+    
+    return 
+    
+  }
+
+  /* TESTE */
+  console.log("INICIANDO TESTES")
