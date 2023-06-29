b18d76415514c949d8266fddb0689456e7db9b18
Vitor Ponce <vitor.ponce20@gmail.com>
2023-06-29 17:31:08 -0300

    [14] Festa dos bixos lvl 1

--- detect_pangram.js
+++ detect_pangram.js
@@ -1,27 +1,53 @@
-var string = "The quick brown fox jumps over the lazy dog."
+const string = "The quick brown fox jumps over the lazy dog."
+var verificaPangran = false;
 console.log(string)
 
 function isPangram(string){
     //... CRIAR UM VETOR QUE VERIFICA TODAS AS LETRAS
-    const isPagran = true;
-    const notIsPagran = false;
+    var isPagran = false;
+       
     let vetor = [];
-    vetor = string.split();
+    vetor = string.split('');
+    
+    console.log("Array de strings")
+    console.log(vetor)
     
     for (i = 0; i < string.length; i++){
       if (vetor[i] === 'a') {
+        console.log(vetor[i])
         console.log("Tem a");
-      } if (vetor[i] === 'b') {
-        console.log("Tem b")
-      }else {
-        
-      } vetor[i]
+        isPagran = true;
+      } else{
+        isPagran = false
+      }
+
+      if (vetor[i] === 'b') {
+        console.log(vetor[i])
+        console.log("Tem b");
+        isPagran = true;
+      } else{
+        isPagran = false
+      }
+
+      if (vetor[i] === 'c') {
+        console.log(vetor[i])
+        console.log("Tem b");
+        isPagran = true;
+      } else{
+        isPagran = false
+      }
+      
       
     }    
     
-    return 
+    return isPagran
     
-  }
+}
+
+isPangram(string);
 
   /* TESTE */
-  console.log("INICIANDO TESTES")
+console.log("INICIANDO TESTES")
+
+ 
+console.log("É PRANGRAM? " + isPangram(string))
